2e7aa6bec3b196546ef85d29d3ca84c45307ca8b
Leon Li <leon.li.1025@gmail.com>
2021-10-04 22:29:16 -0400

    implement Mul for BigDecimal

--- src/utils/big_decimal.rs
+++ src/utils/big_decimal.rs
@@ -1,7 +1,7 @@
 use std::cmp::PartialEq;
 use std::fmt::{Display, Formatter, Result};
 use std::iter::Sum;
-use std::ops::Add;
+use std::ops::{Add, Mul};
 
 #[derive(Debug)]
 pub struct BigDecimal(pub Vec<u8>);
@@ -219,5 +219,34 @@ fn sum_big_decimal_correctness() {
         .map(|x| BigDecimal::from_str(&x))
         .sum::<BigDecimal>(),
         BigDecimal::from_str("181151515151515151512")
+    );
+}
+
+impl Mul<BigDecimal> for BigDecimal {
+    type Output = Self;
+    fn mul(self, other: Self) -> Self {
+        self.0
+            .iter()
+            .map(|v| other.0.iter().map(move |w| v * w))
+            .enumerate()
+            .map(|(i, v_i)| {
+                v_i.enumerate()
+                    .map(|(j, vw)| Self::from_str(&(vw.to_string() + &"0".repeat(i + j))))
+                    .sum::<BigDecimal>()
+            })
+            .sum::<BigDecimal>()
+    }
+}
+
+#[test]
+fn mul_big_decimal_correctness() {
+    assert_eq!(
+        BigDecimal::from_str("2") * BigDecimal::from_str("2"),
+        BigDecimal::from_str("4")
+    );
+    assert_eq!(
+        BigDecimal::from_str("46488416629377247632918291158238061014866615871675152913624137303215071933136")
+        * BigDecimal::from_str("10183905701066272406597175934640315096969880045504139889331864776427950304382965949295737413038440063537216904716682"),
+        BigDecimal::from_str("473433651145459055497486344726446507336504084196668109294981229997455186743014814867177030194263939656882480956773961004110243846170516927332198657059883076562417486973363788367401114127774752")
     );
 }
